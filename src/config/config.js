7f7cd4881a1d46bafcf7ed63beff791b9bacceeb
Ndema Emmanuel <ndemaemmanuel82@gmail.com>
2024-09-09 16:37:12 -0700

    Normalized db connection-2

--- src/config/config.js
+++ src/config/config.js
@@ -4,21 +4,21 @@ module.exports = {
   development: {
     username: process.env.DB_USER || 'root',  // Fallback to 'root' if env var not set
     password: process.env.DB_PASSWORD || 'Emmanuel2002.',  // Fallback to hardcoded password
-    database: process.env.DB_NAME || 'farmsmart',
+    database: process.env.DB || 'farmsmart',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql'
   },
   test: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'Emmanuel2002.',
-    database: process.env.DB_NAME || 'farmsmart',
+    database: process.env.DB || 'farmsmart',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql'
   },
   production: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'Emmanuel2002.',
-    database: process.env.DB_NAME || 'farmsmart',
+    database: process.env.DB || 'farmsmart',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql'
   }
